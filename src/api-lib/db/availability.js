e69e5419d950d6127e2abfff2bb10b60af9f907a
Ben <ben.basque@icloud.com>
2022-10-22 12:54:06 -0700

    Adding comments to database functions

--- src/api-lib/db/availability.js
+++ src/api-lib/db/availability.js
@@ -3,10 +3,8 @@ import { Group, Availability } from '../model';
 export const addAvailabilityToGroup = async ({ groupName, availability }) => {
   // Creates availability then adds availabilities ID to the group 'availabilities' array
   // If theres an error function will return true
-  let error = false;
   const model = new Availability(availability);
-  error = await model
-    .save()
+  const error = await model.save()
     .then((savedDoc) => {
       Group.findOneAndUpdate(
         { name: groupName },
