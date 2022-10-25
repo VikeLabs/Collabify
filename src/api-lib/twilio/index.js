f152ca2d8c29db51ec1b8f9d7562f9745da8b2a4
Ben <ben.basque@icloud.com>
2022-10-25 16:16:40 -0700

    Setup twilio

---
+++ src/api-lib/twilio/index.js
@@ -0,0 +1,15 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const fromNumber = process.env.FROM_NUMBER;
+const client = require('twilio')(accountSid, authToken);
+
+export const sendText = (to, body) => {
+    client.messages
+    .create({
+        from: fromNumber,
+        body,
+        to
+    })
+    .then(message => console.log(message.sid))
+    .catch(err => console.error(err))
+}
