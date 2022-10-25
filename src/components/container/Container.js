9447e939f31b5e313993a07f265be8aa8d9c681f
Ben <ben.basque@icloud.com>
2022-10-24 20:53:36 -0700

    minor refactor

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -4,8 +4,10 @@ import style from 'styles/components/container/container.module.css';
 export const Container = ({ header, children }) => {
   return (
     <section className={style.container}>
-      <div className={style.child}>
+      <header className={style.headerContainer}>
         <h1 className={style.header}>{header}</h1>
+      </header>
+      <div className={style.child}>
 
         {/* styling for children node is NOT handled by this component */}
         <div className={style.content}>{children}</div>
