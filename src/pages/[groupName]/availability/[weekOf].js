2dd97fa0030a594da349f5a7168cbf951e2d96ba
Minh To <toanhminh0412@gmail.com>
2022-10-26 15:46:18 -0700

    Resolve issue 10, style Availability page

--- src/pages/[groupName]/availability/[weekOf].js
+++ src/pages/[groupName]/availability/[weekOf].js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 
 import {AvailabilityCalendar} from '../../../components/AvailabilityCalendar';
 import { AVAILABILITY } from '../../../constants';
-import { Container } from 'components/container/Container';
-import style from 'styles/components/availability.module.css'
+import { Container } from 'components/Container';
+import style from 'styles/pages/availability.module.css'
 import utilities from 'styles/components/utilities.module.css'
 
 export default function Availability() {
@@ -17,12 +17,6 @@ export default function Availability() {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  // User information validator
-  const [nameError, setNameError] = useState(false)
-  const [numberError, setNumberError] = useState(false)
-  const [nameErrorMsg, setNameErrorMsg] = useState("")
-  const [numberErrorMsg, setNumberErrorMsg] = useState("")
-
   // API validator
   const [hasError, setHasError] = useState(false);
 
@@ -30,28 +24,6 @@ export default function Availability() {
 
   // Save the selected availability
   const saveAvailability = () => {
-    // Validate name: Not empty
-    if (name === "") {
-      setNameError(true)
-      setNameErrorMsg("Name cannot be empty")
-      return;
-    }
-    setNameError(false)
-    setNameErrorMsg("")
-
-    // Validate phone number: Length of 10, contains all numbers
-    if (number.length !== 10) {
-      setNumberError(true)
-      setNumberErrorMsg("Phone number has to be 10 digits")
-      return;
-    } else if (!number.match(/[0-9]+/)) {
-      setNumberError(true)
-      setNumberErrorMsg("Phone number has to be all numbers")
-      return;
-    }
-    setNumberError(false)
-    setNumberErrorMsg("")
-
     // Send request to API
     fetch(AVAILABILITY, {
       method: 'POST',
@@ -78,37 +50,38 @@ export default function Availability() {
         {hasError && (
           <Alert severity='error'>{hasError.message}</Alert>
         )}
-        <Typography variant='h5' className={[style.availabilityHeading, utilities.marginBottom1]}>AVAILABILITY: 
-        <span className={style.availabilityText}> Click on the start time and drag to the end time</span>
+        <Typography sx={{ fontWeight: 600}} variant='h5' className={[style.availabilityHeading, utilities.marginBottom1]}>AVAILABILITY: 
+        <span className={style.availabilityText}>Only select when you are available</span>
         </Typography>
 
         <AvailabilityCalendar weekOf={weekOf} times={times} updateTimes={updateTimes}/>
         <div>
-          <div className={utilities.widthFit}>
-            <Typography variant='h5' className={[style.availabilityHeading, utilities.marginTop1, utilities.marginBottom1]}>INFORMATION:</Typography>
-            <div className={[utilities.displayFlex, utilities.flexRow, utilities.flexWrap]}>
+          <div>
+            <Typography sx={{ fontWeight: 600}} variant='h5' className={[style.availabilityHeading, utilities.marginTop1, utilities.marginBottom1]}>INFORMATION:</Typography>
+            <div className={utilities.textCenter}>
               <TextField
                 required
                 id="outlined-required"
                 label="Name"
                 placeholder='Your name'
                 onChange={(e) => setName(e.target.value)}
-                helperText={nameError ? nameErrorMsg : "This field is required"}
-                error={nameError}
+                className={[style.infoTextField]}
+                error={name===""}
               />
               <TextField
                 required
                 id="outlined-required"
-                label="Phone number"
+                label="Phone number (10 digits)"
                 placeholder='Your phone number'
                 onChange={(e) => setNumber(e.target.value)}
-                helperText={numberError ? numberErrorMsg : "This field is required"}
-                className={utilities.marginLeft1}
-                error={numberError}
+                className={[style.infoTextField]}
+                error={number.length != 10 || !number.match(/^\d+$/)}
               />
             </div>
           </div>
-          <Button variant="contained" onClick={saveAvailability} className={[utilities.marginTop2, utilities.fontSize110]}>Save availability</Button>
+          <div className={utilities.textCenter}>
+            <Button variant="contained" onClick={saveAvailability} className={style.saveButton} disabled={name==="" || number.length != 10 || !number.match(/^\d+$/) }>Save availability</Button>
+          </div>
         </div>
       </Container>
     </div>
