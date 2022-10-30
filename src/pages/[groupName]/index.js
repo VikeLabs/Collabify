b472e8b33f257023eb666eec6c55433f6bdca2f3
Hal <haln_01@proton.me>
2022-10-29 23:47:02 -0700

    reformated code

--- src/pages/[groupName]/index.js
+++ src/pages/[groupName]/index.js
@@ -1,4 +1,11 @@
-import { Alert, Box, IconButton, Skeleton, TextField, Typography } from '@mui/material';
+import {
+  Alert,
+  Box,
+  IconButton,
+  Skeleton,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 import { useAsyncFetch } from '../../hooks';
 import { GROUP } from '../../constants';
@@ -9,8 +16,8 @@ import { GroupBanner } from 'components/GroupBanner';
 import { GroupCalendar } from 'components/GroupCalendar';
 import { CopyAllOutlined } from '@mui/icons-material';
 import { getTodaysDate } from 'helper/getTodaysDate';
-import utilities from 'styles/utilities.module.css'
-import style from 'styles/pages/groupHome.module.css'
+import utilities from 'styles/utilities.module.css';
+import style from 'styles/pages/groupHome.module.css';
 
 export default function GroupHome() {
   const router = useRouter();
@@ -25,24 +32,37 @@ export default function GroupHome() {
   const copyLink = () => {
     navigator.clipboard.writeText(
       `localhost:3000/${groupName}/availability/${getTodaysDate()}`
-    )
-    alert('Link Copied!')
-  }
+    );
+    alert('Link Copied!');
+  };
 
   return (
     <Container header={group?.name}>
-      <GroupBanner icon='Today'/>
-      <br/>
-      <GroupCalendar events={[]} createEvent={()=> console.log('')} />
-      <br/>
-      <Typography variant='h5' className={[utilities.heading, utilities.marginBottom1]}>AVAILABILITY LINK: 
-        <span className={utilities.subHeading}> Send to your group members to get results</span>
+      <GroupBanner icon='Today' />
+      <br />
+      <GroupCalendar
+        events={[]}
+        createEvent={() => console.log('')}
+      />
+      <br />
+      <Typography
+        variant='h5'
+        className={[utilities.heading, utilities.marginBottom1]}
+      >
+        AVAILABILITY LINK:
+        <span className={utilities.subHeading}>
+          {' '}
+          Send to your group members to get results
+        </span>
       </Typography>
       <Box className={style.container}>
         <IconButton onClick={copyLink}>
           <CopyAllOutlined />
         </IconButton>
-        <Box className={style.linkContainer} onClick={copyLink}>
+        <Box
+          className={style.linkContainer}
+          onClick={copyLink}
+        >
           <Typography>
             colab-calendar.com/{groupName}/availability/{getTodaysDate()}
           </Typography>
