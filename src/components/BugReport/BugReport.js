c0b666ec4b94f281bd58218b351e883b28e751d2
HalN <haln_01@proton.me>
2022-11-02 20:22:29 -0700

    fix: uri directly in href

--- src/components/BugReport/BugReport.js
+++ src/components/BugReport/BugReport.js
@@ -3,15 +3,14 @@ import { BugReport as Bug } from '@mui/icons-material';
 
 import style from 'styles/components/bugReport.module.css';
 
-const GOOGLE_FORM =
-  'https://docs.google.com/forms/d/1qFzUXBbJDWL8OlkZ0rvqI8HatLYfRromx_anyjk0YmQ/prefill';
-
 export const BugReport = () => {
   return (
     <Tooltip title='Bug, feedback, suggestions!'>
       <a
         className={style.bug}
-        href={GOOGLE_FORM}
+        href={
+          'https://docs.google.com/forms/d/1qFzUXBbJDWL8OlkZ0rvqI8HatLYfRromx_anyjk0YmQ/prefill'
+        }
         target='_blank'
       >
         <Bug />
