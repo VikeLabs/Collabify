fcfb06e90a7f9e35212e12faa2910ad5171f4686
Hal <haln_01@proton.me>
2022-11-03 11:57:03 -0700

    fix: background-color for timetable

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,19 +6,19 @@ import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
 import 'styles/globals.css';
+import 'styles/fullCalendar/fullCalendar.css';
 import { THEME_STORED } from 'constants/index.js';
 
 function MyApp({ Component, pageProps }) {
   // Gets theme from local storage (personalized theme for the site)
-  const themeInStorage = typeof window !== 'undefined' ? localStorage.getItem(THEME_STORED) : null
-  const theme = createTheme(getColorPalette(themeInStorage ?? 'default'))
+  const themeInStorage =
+    typeof window !== 'undefined' ? localStorage.getItem(THEME_STORED) : null;
+  const theme = createTheme(getColorPalette(themeInStorage ?? 'default'));
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <GlobalStyle />
-      <Component
-        {...pageProps}
-      />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
