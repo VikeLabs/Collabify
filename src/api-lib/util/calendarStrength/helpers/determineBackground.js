393188c8c404f67b39db8538f3e849ed931144e8
HalN <haln_01@proton.me>
2022-11-13 13:13:35 -0800

    refactor: sep colors and functions for testings

--- src/api-lib/util/calendarStrength/helpers/determineBackground.js
+++ src/api-lib/util/calendarStrength/helpers/determineBackground.js
@@ -1,14 +1,25 @@
-export const determineBackground = (namesLength, peopleLength) => {
-  const percent = (namesLength / peopleLength) * 100;
-  let color = 'transparent';
-  if (percent > 75) {
-    color = '#228b22';
-  } else if (percent > 50) {
-    color = '#48B613';
-  } else if (percent > 25) {
-    color = '#77dd77';
-  } else if (percent > 0) {
-    color = '#aef5ae';
-  }
-  return color;
+const c = {
+  transparent: 'transparent',
+  gt75: '#228b22',
+  gt50: '#48B613',
+  gt25: '#77dd77',
+  gt0: '#aef5ae',
 };
+
+const determineBackground = (namesLength, peopleLength) => {
+  const percent = (namesLength / peopleLength) * 100;
+
+  if (percent > 75) {
+    return c.gt75;
+  } else if (percent > 50) {
+    return c.gt50;
+  } else if (percent > 25) {
+    return c.gt25;
+  } else if (percent > 0) {
+    return c.gt0;
+  }
+
+  return c.transparent;
+};
+
+export { c as bgColors, determineBackground };
