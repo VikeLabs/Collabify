ab784bc2f33c82ff03a6330948ec29d3647538e0
HalN <haln_01@proton.me>
2022-11-13 14:48:42 -0800

    refactor: throws InvalidArgument exceptions

--- src/api-lib/util/calendarStrength/helpers/determineBackground.js
+++ src/api-lib/util/calendarStrength/helpers/determineBackground.js
@@ -1,4 +1,6 @@
-const c = {
+import { InvalidArguments } from '../calendarStrengthExceptions';
+
+const _c = {
   transparent: 'transparent',
   gt75: '#228b22',
   gt50: '#48B613',
@@ -7,19 +9,34 @@ const c = {
 };
 
 const determineBackground = (namesLength, peopleLength) => {
-  const percent = (namesLength / peopleLength) * 100;
-
-  if (percent > 75) {
-    return c.gt75;
-  } else if (percent > 50) {
-    return c.gt50;
-  } else if (percent > 25) {
-    return c.gt25;
-  } else if (percent > 0) {
-    return c.gt0;
+  if (typeof namesLength !== 'number' || typeof peopleLength !== 'number') {
+    throw new TypeError(
+      `expected [number:number], got [${typeof namesLength}:${typeof peopleLength}]`
+    );
   }
 
-  return c.transparent;
+  if (namesLength > peopleLength) {
+    let err = `_namesLength_ is greater than _peopleLength_.\n`;
+    err += `got [${namesLength}:${peopleLength}] ([namesLength:peopleLength])`;
+    throw new InvalidArguments(err);
+  }
+
+  const percent = (namesLength / peopleLength) * 100;
+
+  if (percent >= 75) {
+    return _c.gt75;
+  } else if (percent >= 50) {
+    return _c.gt50;
+  } else if (percent >= 25) {
+    return _c.gt25;
+  } else if (percent > 0) {
+    return _c.gt0;
+  }
+
+  return _c.transparent;
 };
 
-export { c as bgColors, determineBackground };
+export {
+  _c as bgColors, // for testings only, this is not to be accessed anywhere else
+  determineBackground,
+};
