1c1b06ecfd5993526e74c11ca7acdf592daa8ca8
Hal <haln_01@proton.me>
2022-11-13 19:28:08 -0800

    refactor: remove redundant checks in isAvailable.js

--- src/api-lib/util/calendarStrength/helpers/isAvailable.js
+++ src/api-lib/util/calendarStrength/helpers/isAvailable.js
@@ -21,16 +21,5 @@ export const isAvailable = (event, avail) => {
     );
   }
 
-  /* BUG: try .. catch, since `avail` is sometimes undefined
-   * Ideally `avail` should not be undefined at all..
-   * I'm leaving this complicated looking piece of code
-   * to do a very simple task here in case
-   * it explodes the program in the future.
-   * -Hal
-   * */
-  try {
-    return event.start >= avail.start && event.end <= avail.end;
-  } catch (e) {
-    throw new InvalidArguments(e.message);
-  }
+  return event.start >= avail.start && event.end <= avail.end;
 };
