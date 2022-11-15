3ec939382a7a88653039e37fe19993f7264867b7
Ben <ben.basque@icloud.com>
2022-11-15 15:51:54 -0800

    Adding saving spinner to createEventModal

--- src/components/GroupCalendar/CreateEventModal.js
+++ src/components/GroupCalendar/CreateEventModal.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Box, Modal, Button, TextField } from '@mui/material';
 import style from 'styles/components/groupCalendar.module.css';
 import utilities from 'styles/utilities.module.css';
+import { Spinner } from 'components/Loading';
 
 export default function CreateEventModal({
   createEvent,
@@ -13,8 +14,10 @@ export default function CreateEventModal({
 }) {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
 
   const createEventClick = () => {
+    setIsSaving(true)
     createEvent({
       title,
       description,
@@ -28,57 +31,60 @@ export default function CreateEventModal({
   };
 
   return (
-    <Modal
-      className={style.modalContainer}
-      open={modalIsOpen}
-      onClose={() => setIsOpen(false)}
-    >
-      <Box className={style.container}>
-        <h1 className={style.title}>
-          ({moment(modalInfo.startStr).format('ddd MM/DD')})
-        </h1>
-        <h2 className={style.title}>
-          {moment(modalInfo.startStr).format('hh:mm A')} -{' '}
-          {moment(modalInfo.endStr).format('hh:mm A')}
-        </h2>
-        <Box className={style.textFieldContainer}>
-          <TextField
-            required
-            variant='filled'
-            label='Title'
-            placeholder='Eg; Weekly meeting'
-            onChange={(e) => setTitle(e.target.value)}
-            className={style.textField}
-          />
-          <TextField
-            required
-            variant='filled'
-            label='Description'
-            placeholder='Eg; Room 174'
-            multiline
-            rows={2}
-            onChange={(e) => setDescription(e.target.value)}
-            className={style.textField}
-          />
-        </Box>
-        <Box className={utilities.buttonContainer}>
-          <Button
-            variant='contained'
-            disabled={title === '' || description === ''}
-            className={utilities.button}
-            onClick={createEventClick}
+    <>
+      <Modal
+        className={style.modalContainer}
+        open={modalIsOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Box className={style.container}>
+          <Spinner isLoading={isSaving}/>
+          <h1 className={style.title}>
+            ({moment(modalInfo.startStr).format('ddd MM/DD')})
+          </h1>
+          <h2 className={style.title}>
+            {moment(modalInfo.startStr).format('hh:mm A')} -{' '}
+            {moment(modalInfo.endStr).format('hh:mm A')}
+          </h2>
+          <Box className={style.textFieldContainer}>
+            <TextField
+              required
+              variant='filled'
+              label='Title'
+              placeholder='Eg; Weekly meeting'
+              onChange={(e) => setTitle(e.target.value)}
+              className={style.textField}
+            />
+            <TextField
+              required
+              variant='filled'
+              label='Description'
+              placeholder='Eg; Room 174'
+              multiline
+              rows={2}
+              onChange={(e) => setDescription(e.target.value)}
+              className={style.textField}
+            />
+          </Box>
+          <Box className={utilities.buttonContainer}>
+            <Button
+              variant='contained'
+              disabled={title === '' || description === ''}
+              className={utilities.button}
+              onClick={createEventClick}
+            >
+              CREATE EVENT
+            </Button>
+          </Box>
+          <a
+            onClick={() => setIsOpen(false)}
+            className={style.closeModal}
           >
-            CREATE EVENT
-          </Button>
+            X
+          </a>
         </Box>
-        <a
-          onClick={() => setIsOpen(false)}
-          className={style.closeModal}
-        >
-          X
-        </a>
-      </Box>
-    </Modal>
+      </Modal>
+    </>
   );
 }
 
