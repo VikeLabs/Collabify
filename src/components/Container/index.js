5da79f92f7c105b34112accd388e8dbd244917c5
HalN <haln_01@proton.me>
2022-11-21 12:38:54 -0800

    refactor: semantic html

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,13 +3,13 @@ import { useDeviceDetect } from 'hooks';
 import * as Icons from '@mui/icons-material';
 import style from 'styles/components/container.module.css';
 
-export const Container = ({ 
-  header = '', 
-  leftIcon = null, 
+export const Container = ({
+  header = '',
+  leftIcon = null,
   leftIconClick,
-  rightIcon = null, 
+  rightIcon = null,
   rightIconClick,
-  children 
+  children,
 }) => {
   const { isMobile } = useDeviceDetect();
   const LeftIconComponent = Icons[leftIcon];
@@ -17,23 +17,35 @@ export const Container = ({
   return (
     <section className={style.container}>
       <header className={style.headerContainer}>
-        {leftIcon && <LeftIconComponent 
-        onClick={leftIconClick}
-        icon={leftIcon} 
-        style={{
-          color: 'white', 
-          position: 'absolute', 
-          marginTop: '0.2em'
-        }}/>}
-        {rightIcon && <RightIconComponent 
-        onClick={rightIconClick}
-        icon={rightIcon} 
-        style={{
-          color: 'white', 
-          position: 'absolute', 
-          marginTop: '0.2em',
-          right: isMobile ? '3vw' : '2.5vw'
-        }}/>}
+        {leftIcon && (
+          <div aria-label='back button'>
+            <LeftIconComponent
+              onClick={leftIconClick}
+              icon={leftIcon}
+              style={{
+                color: 'white',
+                position: 'absolute',
+                marginTop: '0.2em',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+        )}
+        {rightIcon && (
+          <div aria-label='create availability'>
+            <RightIconComponent
+              onClick={rightIconClick}
+              icon={rightIcon}
+              style={{
+                color: 'white',
+                position: 'absolute',
+                marginTop: '0.2em',
+                right: isMobile ? '3vw' : '2.5vw',
+                cursor: 'pointer',
+              }}
+            />
+          </div>
+        )}
         <h1 className={style.header}>{header}</h1>
       </header>
       <div className={style.child}>
