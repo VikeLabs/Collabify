e7431fa9892f658445d0e2563941b9c1fbd3b6f0
BenReichwein <ben.basque@icloud.com>
2022-11-22 23:31:58 -0800

    Drop down menu added

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,8 @@
-import { MuiIcon } from 'components/MuiIcon';
 import { useDeviceDetect } from 'hooks';
 import * as Icons from '@mui/icons-material';
 import style from 'styles/components/container.module.css';
+import { useState } from 'react';
+import { Divider, Menu, MenuItem } from '@mui/material';
 
 export const Container = ({ 
   header = '', 
@@ -9,17 +10,32 @@ export const Container = ({
   leftIconClick,
   rightIcon = null, 
   rightIconClick,
-  children 
+  children,
+  menu = null
 }) => {
   const { isMobile } = useDeviceDetect();
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+
   const LeftIconComponent = Icons[leftIcon];
   const RightIconComponent = Icons[rightIcon];
+  const MenuIconComponent = Icons['ExpandMore']
   return (
     <section className={style.container}>
       <header className={style.headerContainer}>
+        {menu && <MenuIconComponent 
+        onClick={(e)=> {
+          setAnchorEl(e.currentTarget);
+          setIsOpen(true)
+        }}
+        style={{
+          color: 'white', 
+          position: 'absolute', 
+          marginTop: '0.2em'
+        }}/>}
         {leftIcon && <LeftIconComponent 
         onClick={leftIconClick}
-        icon={leftIcon} 
         style={{
           color: 'white', 
           position: 'absolute', 
@@ -27,7 +43,6 @@ export const Container = ({
         }}/>}
         {rightIcon && <RightIconComponent 
         onClick={rightIconClick}
-        icon={rightIcon} 
         style={{
           color: 'white', 
           position: 'absolute', 
@@ -36,6 +51,29 @@ export const Container = ({
         }}/>}
         <h1 className={style.header}>{header}</h1>
       </header>
+      <Menu
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        open={isOpen}
+        onClose={()=> setIsOpen(false)}
+        className={style.menu}
+      >
+        {menu?.map((e, index) => {
+          const Icon = Icons[e.icon]
+          return (
+            <div key={index}>
+              <Divider light/>
+              <MenuItem onClick={e.onClick}
+              >
+                <Icon/>
+                <h4>{e.text}</h4>
+              </MenuItem>
+            </div>
+          )
+        })}
+      </Menu>
       <div className={style.child}>
         {/* styling for children node is NOT handled by this component */}
         <div className={style.content}>{children}</div>
