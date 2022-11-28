0a62a36be83f8b7af2fdc2db1d461dcd2d8bf5db
HalN <haln_01@proton.me>
2022-11-27 16:39:11 -0800

    resolved merge conflict with main

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,8 @@
-import { MuiIcon } from 'components/MuiIcon';
 import { useDeviceDetect } from 'hooks';
 import * as Icons from '@mui/icons-material';
 import style from 'styles/components/container.module.css';
+import { useState } from 'react';
+import { Divider, Menu, MenuItem } from '@mui/material';
 
 export const Container = ({
   header = '',
@@ -10,44 +11,82 @@ export const Container = ({
   rightIcon = null,
   rightIconClick,
   children,
+  menu = null,
 }) => {
   const { isMobile } = useDeviceDetect();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
   const LeftIconComponent = Icons[leftIcon];
   const RightIconComponent = Icons[rightIcon];
+  const MenuIconComponent = Icons['ExpandMore'];
   return (
     <section className={style.container}>
       <header className={style.headerContainer}>
-        {leftIcon && (
-          <div aria-label='back button'>
-            <LeftIconComponent
-              onClick={leftIconClick}
-              icon={leftIcon}
-              style={{
-                color: 'white',
-                position: 'absolute',
-                marginTop: '0.2em',
-                cursor: 'pointer',
-              }}
-            />
-          </div>
+        {/* MENU */}
+        {menu && (
+          <MenuIconComponent
+            onClick={(e) => {
+              setAnchorEl(e.currentTarget);
+              setIsOpen(true);
+            }}
+            style={{
+              color: 'white',
+              position: 'absolute',
+              marginTop: '0.2em',
+            }}
+          />
         )}
+
+        {/* LEFT BUTTON */}
+        {leftIcon && (
+          <LeftIconComponent
+            onClick={leftIconClick}
+            style={{
+              color: 'white',
+              position: 'absolute',
+              marginTop: '0.2em',
+            }}
+          />
+        )}
+
+        {/* RIGHT BUTTON */}
         {rightIcon && (
-          <div aria-label='create availability'>
-            <RightIconComponent
-              onClick={rightIconClick}
-              icon={rightIcon}
-              style={{
-                color: 'white',
-                position: 'absolute',
-                marginTop: '0.2em',
-                right: isMobile ? '3vw' : '2.5vw',
-                cursor: 'pointer',
-              }}
-            />
-          </div>
+          <RightIconComponent
+            onClick={rightIconClick}
+            style={{
+              color: 'white',
+              position: 'absolute',
+              marginTop: '0.2em',
+              right: isMobile ? '3vw' : '2.5vw',
+            }}
+          />
         )}
         <h1 className={style.header}>{header}</h1>
       </header>
+      <Menu
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className={style.menu}
+      >
+        {menu?.map((e, index) => {
+          const Icon = Icons[e.icon];
+          return (
+            <div key={index}>
+              <Divider light />
+              <MenuItem onClick={e.onClick}>
+                <Icon />
+                <h4>{e.text}</h4>
+              </MenuItem>
+            </div>
+          );
+        })}
+      </Menu>
       <div className={style.child}>
         {/* styling for children node is NOT handled by this component */}
         <div className={style.content}>{children}</div>
