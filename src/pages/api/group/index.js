f9da15b45b82426de92e30374b4d1dcdf1178c63
Hal <haln_01@proton.me>
2022-12-03 13:53:37 -0800

    feat: createGroup now handles private

--- src/pages/api/group/index.js
+++ src/pages/api/group/index.js
@@ -10,17 +10,31 @@ export default async function handler(req, res) {
     return;
   }
 
+  /* EXTRACTING GROUP INFO */
+  const group = JSON.parse(body);
+  // if group is private and no password is provided
+  if (group.isPrivate && !group.password) {
+    res.status(400).json({
+      message: 'Group is private but no password is provided',
+    });
+    return;
+  }
+
+  /* SAVE TO DB */
   await dbConnect();
 
   try {
-    const group = JSON.parse(body);
     const { error, groupID } = await createGroup({ group });
-    if (error === true) sendDatabaseError(res);
-    else
-      res.status(200).json({
-        ok: true,
-        groupID,
-      });
+
+    if (error === true) {
+      sendDatabaseError(res);
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      groupID,
+    });
   } catch (err) {
     sendRequestError(res, err);
   }
