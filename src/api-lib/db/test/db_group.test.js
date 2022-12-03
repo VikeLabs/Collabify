17b946cb1400b1c42027188d943a6d6c8b3755f6
Hal <haln_01@proton.me>
2022-12-03 13:52:05 -0800

    test: test suites for createGroup

--- src/api-lib/db/test/db_group.test.js
+++ src/api-lib/db/test/db_group.test.js
@@ -2,10 +2,11 @@ import { it, expect, describe, beforeAll, afterAll } from '@jest/globals';
 import { createGroup } from '../group';
 import dbConnect from '../../dbConnect.js';
 import ld from 'lodash';
-import { Group, GroupPw } from '../../model';
+import { Group, GroupPasswords } from '../../model';
 
 import { publicGroup, privateGroup } from './mockdata/db_group_mock';
 
+let savedID = []; // test groups to be deleted
 beforeAll(async () => {
   await dbConnect();
 });
@@ -17,16 +18,15 @@ beforeEach(() => {
   publicGroup_clone = ld.cloneDeep(publicGroup);
 });
 
-let savedID = []; // test groups to be deleted
 afterAll(async () => {
   // clean up
   await Group.deleteMany({ _id: { $in: savedID } });
-  await GroupPw.deleteMany({ group: { $in: savedID } });
+  await GroupPasswords.deleteMany({ group: { $in: savedID } });
   savedID = [];
 });
 
 describe('/api-lib/db/group test suite', () => {
-  describe('test for `private` functionality', () => {
+  describe('public group behaviours', () => {
     it('saves a public group', async () => {
       const { error, groupID } = await createGroup({
         group: publicGroup_clone,
@@ -34,42 +34,55 @@ describe('/api-lib/db/group test suite', () => {
       expect(error).toBe(false);
       savedID.push(groupID);
 
-      // query db for id
       Group.findById(groupID, (_, doc) => {
         expect(doc.isPrivate).toBe(false);
       });
     });
+  });
 
-    describe('saves a private group', () => {
-      it('throws an error for `sendRequestError`', async () => {
+  describe('private group behaviours', () => {
+    describe('_groups_ collection', () => {
+      it('_isPrivate_', async () => {
+        const { error, groupID } = await createGroup({
+          group: privateGroup_clone,
+        });
+        expect(error).toBe(false);
+        savedID.push(groupID);
+        Group.findById(groupID, (_, doc) => {
+          expect(doc.isPrivate).toBe(true);
+        });
+      });
+    });
+
+    describe('_group_passwords_ collection', () => {
+      it('returns a (bool) error for _sendRequestError_', async () => {
         publicGroup_clone.isPrivate = true;
 
         const { error, groupID } = await createGroup({
           group: publicGroup_clone,
         });
+
         expect(error).toBe(true);
         expect(groupID).toBe(null);
       });
 
-      it('deletes password after hashed', async () => {
-        const { error, groupID } = await createGroup({
+      it('deletes password from _group_ object after encryption', async () => {
+        const { groupID } = await createGroup({
           group: privateGroup_clone,
         });
         savedID.push(groupID);
 
-        expect(error).toBe(false);
         expect(privateGroup_clone.password).toBeUndefined();
       });
 
-      it('`isPrivate` is true', async () => {
-        const { error, groupID } = await createGroup({
+      it('saves password if no error is thrown', async () => {
+        const { groupID } = await createGroup({
           group: privateGroup_clone,
         });
-        expect(error).toBe(false);
         savedID.push(groupID);
 
-        Group.findById(groupID, (_, doc) => {
-          expect(doc.isPrivate).toBe(true);
+        GroupPasswords.findOne({ group: groupID }, (_, matched) => {
+          expect(matched.group.toString()).toBe(groupID.toString());
         });
       });
     });
