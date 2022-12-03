bb98fc6ce5514de5b071004c7b2dcd2eb8b1c0d6
Hal <haln_01@proton.me>
2022-12-03 14:12:44 -0800

    fix: reconfig jest to support absolute import

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
-module.exports = {
-  setupFiles: ['dotenv/config'],
+const nextJest = require('next/jest');
+
+const nextJestConfig = nextJest({ dir: './' });
+
+const jestConfig = {
   testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    'api-lib(.*)': '<rootDir>/src/api-lib/$1',
+    'components(.*)': '<rootDir>/src/components/$1',
+    'hooks(.*)': '<rootDir>/src/hooks/$1',
+    'pages(.*)': '<rootDir>/src/pages/$1',
+  },
 };
+
+module.exports = nextJestConfig(jestConfig);
