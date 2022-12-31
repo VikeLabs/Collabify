9560916bbd6e71f7d0ba7c071213b862a6f1e6ed
Hal <haln_01@proton.me>
2022-12-31 13:59:52 -0800

    fix: mockFetch in LogInForm

--- src/api-lib/auth/verifyJwt.js
+++ src/api-lib/auth/verifyJwt.js
@@ -3,12 +3,11 @@ import { UnauthorizedError } from 'api-lib/util/exceptions';
 
 const SECRET = process.env.PRIVATE_GROUP_SECRET;
 
-export const verifyJwt = (token) => {
-  return new Promise((resolve, reject) => {
-    verify(token, SECRET, (err, decodedToken) => {
-      if (err) return reject(new UnauthorizedError(err));
-
-      return resolve(decodedToken);
-    });
+export const verifyJwt = (token, callback) => {
+  verify(token, SECRET, (err, decodedToken) => {
+    if (err) {
+      throw new UnauthorizedError();
+    }
+    callback(decodedToken);
   });
 };
