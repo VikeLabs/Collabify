42a707a874e25b85f02016427e669d66ca566503
AniTheNinja <misha.loura@gmail.com>
2023-01-02 12:49:10 -0800

    Added front end UI and made new css file.

--- src/components/Home/LogInForm.jsx
+++ src/components/Home/LogInForm.jsx
@@ -1,11 +1,40 @@
 import { useState } from 'react';
 
+import { Container, TextField } from '@mui/material';
+import { Button } from '@mui/material';
+
+import utilities from 'styles/utilities.module.css';
+import styles from 'styles/components/LogInForm.module.css';
+
 /**
  * setIsAuth: setState<boolean>
  *            -- call `setIsAuth(() => true)` on success
  */
 export const LogInForm = ({ setIAuth }) => {
-  //
+  {/* Entering Password To Authenticate*/}
+  return(
+    <section className={styles.authPasswordContainer}>
+      <h1 className={utilities.heading} >Private Group Authentication Required:</h1>
+      {/* Text field to collect password*/}
+
+      <div>
+      <TextField className={styles.inputFieldAuth}
+              label='Password (enter to gain access)'
+              variant='filled'
+              type='password'
+              size='large'      
+            />
+      </div>
+      
+      <div className={styles.containerButton}>
+        <Button className={styles.buttonSubmitAuth}  size='large' variant="contained">
+          Submit
+        </Button>
+      </div>
+
+    </section>
+  )
+
 };
 
 const mockFetch = (fetchStatus) => {
