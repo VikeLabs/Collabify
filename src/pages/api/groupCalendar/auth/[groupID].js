b3e107339294ea9cfaae32ebaa7cf00f03d52696
Hal <haln_01@proton.me>
2023-01-05 16:49:32 -0800

    fix: desctructring password in request body

--- src/pages/api/groupCalendar/auth/[groupID].js
+++ src/pages/api/groupCalendar/auth/[groupID].js
@@ -2,32 +2,41 @@ import { validateGroupPassword } from 'api-lib/db/private_password';
 import { ApiError } from 'api-lib/util/exceptions';
 import { signJWT } from 'api-lib/auth';
 import { Cookie } from 'api-lib/requests/cookie';
+import dbConnect from 'api-lib/dbConnect';
 
-export default async function (req, res) {
-  const { groupID, password } = req.query;
-  if (!groupID || !password) {
-    res.status(400).json({ message: 'missing group ID and/or password' });
-    return;
-  }
+export default function (req, res) {
+  return new Promise(async (resolve) => {
+    const { groupID } = req.query;
+    const { password } = req.body;
 
-  switch (req.method) {
-    case 'POST': {
-      validateGroupPassword({ groupID, password }, (error) => {
-        if (error !== null || error instanceof ApiError) {
-          res.status(error.statusCode).json({ message: error.message });
-          return;
-        }
-
-        // group validated, send back credentials
-        const cookie = Cookie.New(req, res);
-        const token = signJWT({ groupID });
-        cookie.setPrivateGroupToken(token);
-
-        return res.status(200).json();
-      });
+    if (password === '' || !password) {
+      res.status(400).json({ message: 'missing password' });
+      return resolve();
     }
-    default: // method not allowed
-      res.status(405).json(null);
-      break;
-  }
+
+    await dbConnect();
+    switch (req.method) {
+      case 'POST':
+        {
+          validateGroupPassword({ groupID, password }, (error) => {
+            if (error !== null || error instanceof ApiError) {
+              res.status(error.statusCode).json({ message: error.message });
+              return resolve();
+            }
+
+            // group validated, send back credentials
+            const cookie = Cookie.New(req, res);
+            const token = signJWT({ groupID });
+            cookie.setPrivateGroupToken(token);
+
+            res.status(200).json();
+            return resolve();
+          });
+        }
+        break;
+      default: // method not allowed
+        res.status(405).json(null);
+        return resolve();
+    }
+  });
 }
