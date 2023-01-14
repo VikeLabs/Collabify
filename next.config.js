34c7e1442eb2c911a15f69686a98328e6d887068
BenReichwein <ben.basque@icloud.com>
2023-01-14 13:53:07 -0800

    Fixing babel error hot fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
-  '@babel/preset-react',
   '@fullcalendar/common',
   '@fullcalendar/daygrid',
   '@fullcalendar/interaction',
