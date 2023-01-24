b43cd7d05b20247d0146f851c3dc40ac626b54c4
Hal <haln_01@proton.me>
2023-01-24 11:32:00 -0800

    refactor: `updateGroup`

--- src/api-lib/db/group/index.ts
+++ src/api-lib/db/group/index.ts
@@ -2,4 +2,3 @@ export * from './createGroup';
 export * from './getAllGroups';
 export * from './getGroupByID';
 export * from './getManyGroups';
-export * from './updateGroup';
--- src/pages/api/groupCalendar/[groupID]/index.ts
+++ src/pages/api/groupCalendar/[groupID]/index.ts
@@ -1,23 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { JsonWebToken } from 'api-lib/auth';
 import { getEvents } from 'api-lib/db/event';
-import { getGroupByID } from 'api-lib/db/group';
+import { getGroupByID, updateGroup } from 'api-lib/db/group';
 import { getAvailabilities } from 'api-lib/db';
+import { Group } from '@prisma/client';
+import prisma from 'api-lib/prisma';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const { method } = req;
-  if (method !== 'GET') {
-    res.status(405).end();
-    return;
-  }
-
   try {
+    const { method } = req;
     const { groupID: _groupID } = req.query;
     const groupID = parseInt(_groupID as string);
 
+    // Get group
     const {
       group,
       error: groupError,
@@ -29,38 +27,54 @@ export default async function handler(
       return;
     }
 
-    /* PRIVATE GROUP VALIDATION */
+    // Handle private group
     if (group.isPrivate) {
-      const token = getAuthToken(req.rawHeaders);
-      if (!token) {
-        res.status(401).end();
+      const authToken = getAuthToken(req.rawHeaders);
+      if (!authToken) {
+        res.status(401).end(); // no token provided in header.
         console.warn(
           `group [${groupID}] is private but no auth token is attached to the request header`
         );
         return;
       }
 
-      const { groupToken, err } = JsonWebToken.getPrivateGroupToken(token);
+      const { groupToken, err } = JsonWebToken.getPrivateGroupToken(authToken);
       if (err) {
-        res.status(err.statusCode).end();
+        res.status(err.statusCode).end(); // token invalid (ie, expired, tampered, etc..)
         return;
       }
 
       if (groupToken !== privateGrToken) {
-        // wrong token
-        res.status(401).end();
+        res.status(401).end(); // wrong token
         return;
       }
     }
 
-    /* PARSE GROUP INFORMATION */
-    const allAvailabilities = await getAvailabilities(groupID);
-    const allEvents = await getEvents(groupID);
+    switch (method) {
+      case 'GET': {
+        // Get group information
+        const allAvailabilities = await getAvailabilities(groupID);
+        const allEvents = await getEvents(groupID);
 
-    res.status(200).json({
-      group,
-      calendarEvents: [...allAvailabilities, ...allEvents],
-    });
+        res.status(200).json({
+          group,
+          calendarEvents: [...allAvailabilities, ...allEvents],
+        });
+        return;
+      }
+
+      case 'PATCH': {
+        const updatedGroup: Group = req.body;
+        const data = { ...group, ...updatedGroup }; // overriding existing group
+        await prisma.group.update({ where: { id: groupID }, data });
+
+        res.status(200).end();
+      }
+
+      default:
+        res.status(405).end();
+        return;
+    }
   } catch (error) {
     res.status(500).end();
     console.log(error);
