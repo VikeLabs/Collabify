c97f5b1c8c1079c8a6012a02c307e5849410950e
Hal <haln_01@proton.me>
2023-01-25 10:10:56 -0800

    refactor: modularized dropdown component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,12 +1,7 @@
-import { useDeviceDetect, useLocalStorage } from 'hooks';
+import { useDeviceDetect } from 'hooks';
 import * as Icons from '@mui/icons-material';
 import style from 'styles/components/container.module.css';
-import { useState } from 'react';
-import { Divider, Menu, MenuItem } from '@mui/material';
-import { IntroTooltip } from 'components/IntroTooltip';
-import { CLOSE_ALL_TOOLTIPS } from 'constants';
 import { AppBar } from '../../../node_modules/@material-ui/core/index';
-import { MenuIcon } from 'components/ContainerIcons/menuIcon';
 import { LeftIcon } from 'components/ContainerIcons/leftRightIcon';
 import { RightIcon } from 'components/ContainerIcons/leftRightIcon';
 
@@ -17,18 +12,13 @@ export const Container = ({
   rightIcon = null,
   rightIconClick,
   children,
-  menu = null,
+  menu,
 }) => {
   const { isMobile } = useDeviceDetect();
-  // Menu drop down
-  const [isOpen, setIsOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
   // showing tooltip
-  const [showTooltip, setShowToolTip] = useLocalStorage(CLOSE_ALL_TOOLTIPS);
   // Icon init
   const LeftIconComponent = Icons[leftIcon];
   const RightIconComponent = Icons[rightIcon];
-  const MenuIconComponent = Icons['Menu'];
 
   return (
     <div>
@@ -36,25 +26,7 @@ export const Container = ({
         className={style.headerContainer}
         position='static'
       >
-        {/*TODO: Talk about having the navbar overtop of child as scroll */}
-        {menu && (
-          <IntroTooltip
-            text='extend for more options'
-            visible={showTooltip === undefined ?? true}
-            close={() => setShowToolTip(false)}
-            closeAll={() => {
-              setShowToolTip(false);
-              localStorage.setItem(CLOSE_ALL_TOOLTIPS, true);
-            }}
-          >
-            <MenuIcon
-              MenuIconComponent={MenuIconComponent}
-              setAnchorEl={setAnchorEl}
-              setShowToolTip={setShowToolTip}
-              setIsOpen={setIsOpen}
-            />
-          </IntroTooltip>
-        )}
+        {menu ? menu : null}
         {leftIcon && (
           <div>
             <LeftIcon
@@ -78,28 +50,6 @@ export const Container = ({
         <h1 className={style.header}>{header}</h1>
       </AppBar>
       {/* Drop down menu */}
-      <Menu
-        anchorEl={anchorEl}
-        getContentAnchorEl={null}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className={style.menu}
-      >
-        {menu?.map((e, index) => {
-          const Icon = Icons[e.icon];
-          return (
-            <div key={index}>
-              <Divider light />
-              <MenuItem onClick={e.onClick}>
-                <Icon />
-                <h4>{e.text}</h4>
-              </MenuItem>
-            </div>
-          );
-        })}
-      </Menu>
       <div className={style.child}>
         {/* styling for children node is NOT handled by this component */}
         <div className={style.content}>{children}</div>
