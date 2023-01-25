76304c7d205ef9d5e8bb3ea4dc8ea95244bd248c
Hal <haln_01@proton.me>
2023-01-25 00:43:14 -0800

    refactor: twillo and event endpoint

--- src/api-lib/twilio/index.ts
+++ src/api-lib/twilio/index.ts
@@ -1,17 +1,17 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const fromNumber = process.env.FROM_NUMBER;
+const from = process.env.FROM_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 
-export const sendText = (to, body) => {
+export const sendText = (to: string, body: string) => {
   client.messages
     .create({
-      from: fromNumber,
+      from,
       body,
       to,
     })
-    .then((message) =>
+    .then((message: any) =>
       console.log(`Text sent successful to SID: ${message.sid}`)
     )
-    .catch((err) => console.error(err));
+    .catch((err: any) => console.error(err));
 };
