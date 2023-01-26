34ac04d694275adc918b66a442b1de7844ddbc43
Hal <haln_01@proton.me>
2023-01-26 00:50:53 -0800

    chore: remove unused files

--- src/components/ContainerIcons/leftRightIcon.js
+++ src/components/ContainerIcons/leftRightIcon.js
@@ -1,29 +1,30 @@
-export const LeftIcon = (props) => {
-  return (
-    <props.LeftIconComponent
-      onClick={props.leftIconClick}
-      icon={props.leftIcon}
-      style={{
-        color: 'white',
-        position: 'absolute',
-        marginTop: '0.2em',
-        cursor: 'pointer',
-      }}
-    />
-  );
-};
-export const RightIcon = (props) => {
-  return (
-    <props.RightIconComponent
-      onClick={props.rightIconClick}
-      icon={props.rightIcon}
-      style={{
-        color: 'white',
-        position: 'absolute',
-        marginTop: '0.2em',
-        right: props.isMobile ? '3vw' : '2.5vw',
-        cursor: 'pointer',
-      }}
-    />
-  );
-};
+// TODO: delete later
+// export const LeftIcon = (props) => {
+//   return (
+//     <props.LeftIconComponent
+//       onClick={props.leftIconClick}
+//       icon={props.leftIcon}
+//       style={{
+//         color: 'white',
+//         position: 'absolute',
+//         marginTop: '0.2em',
+//         cursor: 'pointer',
+//       }}
+//     />
+//   );
+// };
+// export const RightIcon = (props) => {
+//   return (
+//     <props.RightIconComponent
+//       onClick={props.rightIconClick}
+//       icon={props.rightIcon}
+//       style={{
+//         color: 'white',
+//         position: 'absolute',
+//         marginTop: '0.2em',
+//         right: props.isMobile ? '3vw' : '2.5vw',
+//         cursor: 'pointer',
+//       }}
+//     />
+//   );
+// };
