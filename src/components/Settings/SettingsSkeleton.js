0aba9c4209da0a4f1d722ac32609f045f2d75c3e
Hal <haln_01@proton.me>
2023-01-26 00:52:47 -0800

    chore: re-adding Skeletons

---
+++ src/components/Settings/SettingsSkeleton.js
@@ -0,0 +1,95 @@
+import { Box, Skeleton } from '@mui/material';
+import { Container } from 'components/Container';
+
+export function SettingsSkeleton() {
+  return (
+    <Container header='loading...'>
+      <Skeleton
+        height={60}
+        width={200}
+      />
+      <Box sx={{ display: 'flex' }}>
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+        <Skeleton
+          height={90}
+          width={75}
+          sx={{ borderRadius: '50%', marginLeft: '10px' }}
+        />
+      </Box>
+      <Skeleton
+        height={60}
+        width={200}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Skeleton
+          height={100}
+          sx={{ borderRadius: '10px', width: '45%' }}
+        />
+        <Skeleton
+          height={100}
+          sx={{ borderRadius: '10px', width: '45%' }}
+        />
+      </Box>
+      <Skeleton
+        height={60}
+        width={200}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Skeleton
+          height={100}
+          sx={{ borderRadius: '10px', width: '48%' }}
+        />
+        <Skeleton
+          height={100}
+          sx={{ borderRadius: '10px', width: '48%' }}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Skeleton
+          height={80}
+          width={200}
+          sx={{ borderRadius: '10px' }}
+        />
+      </Box>
+    </Container>
+  );
+}
