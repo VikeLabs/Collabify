350aea03daea67a863d850a8bb8f1c1683b0bbd4
BenReichwein <ben.basque@icloud.com>
2023-02-01 20:50:04 -0800

    Trouble shooting prisma prod issue

--- src/api-lib/prisma.ts
+++ src/api-lib/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('PRISMA LOADED ON PROD!')
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
