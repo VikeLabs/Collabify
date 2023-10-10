e39ff15dc3e2c08b6b482d548511c001fa3ec51c
Ben <reichweinben@gmail.com>
2023-10-10 16:25:07 -0700

    Privacy Policy and TOS

---
+++ src/pages/privacyPolicy.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+     <h1>Collabify Privacy Policy</h1>
+
+      <p>Effective Date: Tue Oct 10, 2023</p>
+
+      <h2>1. Information We Collect</h2>
+
+      <p>Collabify may collect and process the following types of personal information:</p>
+
+      <ul>
+          <li>Contact Information, including email address.</li>
+          <li>Usage Data, including information about how you use our website.</li>
+      </ul>
+
+      <h2>2. How We Use Your Information</h2>
+
+      <p>We may use the information we collect from you for various purposes, including:</p>
+
+      <ul>
+          <li>To provide, maintain, and improve our services.</li>
+          <li>To respond to your inquiries, comments, or questions.</li>
+          <li>To send emails and other communications.</li>
+      </ul>
+
+      <h2>3. Cookies and Similar Technologies</h2>
+
+      <p>Collabify may use cookies and similar tracking technologies to enhance user experience. You can set your browser to refuse all or some browser cookies or to alert you when websites set or access cookies.</p>
+
+      <h2>4. Third-Party Links</h2>
+
+      <p>Our website may contain links to third-party websites. We have no control over the content and privacy policies of these sites, and we are not responsible for their practices.</p>
+
+      <h2>5. Data Security</h2>
+
+      <p>We have implemented measures designed to secure your personal information from accidental loss and unauthorized access, use, alteration, and disclosure.</p>
+
+      <h2>6. Changes to Our Privacy Policy</h2>
+
+      <p>Collabify reserves the right to update or change this Privacy Policy at any time. Any changes will be effective immediately upon posting to our website.</p>
+
+      <h2>7. Contact Information</h2>
+
+      <p>If you have any questions or concerns about this Privacy Policy, please contact us at privacy@collabify.space.</p>
+
+      <p>Last Updated: Tue Oct 10, 2023</p>
+    </div>
+  )
+}
